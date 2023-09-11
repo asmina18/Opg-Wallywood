3c049c6d2500463ea1a07479b04c9eb668c122ac
asmina18 <asmina18@mail.ru>
2023-09-11 19:36:54 +0200

    new

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+
+export const Footer = () => {
+
+    return (
+        <footer>
+            <div>
+                <h3>Wallywood</h3>
+                <address>Øster Uttrupvej 1<br />
+                    9000 Aalborg
+                </address>
+            </div>
+            <div>
+                CVR: 12345678<br />
+                MAIL: info@wallywood.dk<br />
+                MOBIL: +45 9812 3456
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import{Link} from 'react-router-dom'
+export function Navigation (){
+
+    return(
+        <nav>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/plakater'>Plakater</Link>
+                <Link to='/omos'>Om os</Link>
+                <Link to='/kontakt'>Kontakt</Link>
+                <Link to='/login'>Login</Link>
+            </div>
+        </nav>
+    )
+}
